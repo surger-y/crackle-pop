00ace4b394271ee35c62d35bb5d9e3d2bf87aba7
surger-y <offline.surgery@gmail.com>
2024-04-12 10:31:51 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const cracklePop = () => {
+    
+    const fullArray = Array
+        .apply(null, Array(100))
+        .map((x, i) => {
+            let value = i+1
+                if ((value % 3) == 0 && (value % 5) == 0){
+                    value = 'CracklePop'
+                } else if ((value % 5) == 0){
+                    value = 'Pop'
+                } else if ((value % 3) == 0){
+                    value = 'Crackle'
+                }
+            return value
+        })
+
+    const displayValues = (arr) => {
+        const container = document.querySelector('.container')
+           
+        for (let i = 0; i < arr.length; i++){
+            const newDiv = document.createElement('div')
+            newDiv.setAttribute('class', 'box')    
+            newDiv.innerText = arr[i].toString()
+            container.appendChild(newDiv)
+        }
+    }
+
+    displayValues(fullArray)
+}
+
+cracklePop()
